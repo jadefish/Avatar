0c6a188cc0ce83adfa2929450d6998c00141e2cb
Nick Miller <nick@nmiller.info>
2018-09-30 12:28:10 -0400

    Add account table schema

---
+++ sql/schema/accounts.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS `accounts` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'UTC timestamp of record creation',
+    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'UTC timestamp of last update',
+    `deleted_at` timestamp NULL DEFAULT NULL COMMENT 'UTC timestamp of soft record deletion',
+    `last_logged_in_at` timestamp NULL DEFAULT NULL COMMENT 'UTC timestamp of last successful login',
+    `name` char(20) CHARACTER SET ascii NOT NULL COMMENT 'Account name, limited to 20 characters',
+    `email` varchar(254) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Account owner''s registered email address',
+    `password` char(60) CHARACTER SET ascii NOT NULL COMMENT 'Password hash',
+    `creation_ip` char(16) CHARACTER SET ascii NOT NULL COMMENT 'IP address of record creation',
+    `last_login_ip` char(16) CHARACTER SET ascii DEFAULT NULL COMMENT 'IP address of last successful login',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `name` (`name`),
+    UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
