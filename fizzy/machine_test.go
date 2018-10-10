3156727f06fc82cc0894e051c9328bb2a9923cb1
Nick Miller <nick@nmiller.info>
2018-10-09 22:24:57 -0400

    Update/add fizzy unit tests

--- fizzy/machine_test.go
+++ fizzy/machine_test.go
@@ -8,41 +8,71 @@ func TestNewMachine(t *testing.T) {
 	m, err := NewMachine("s0")
 
 	if err != nil {
-		t.Error("TestNewMachine cannot construct a new machine")
+		t.Error("NewMachine failed to construct a new machine")
 	}
 
 	if len(m.states) < 1 {
-		t.Error("TestNewMachine constructed an invalid machine")
+		t.Error("NewMachine constructed a machine with no states")
+	}
+
+	_, err = NewMachine("")
+
+	if err == nil {
+		t.Error("NewMachine allowed creation of a nameless state")
 	}
 }
 
-func TestAddState(t *testing.T) {
-	m, err := NewMachine("s0")
+func TestMachine_AddState(t *testing.T) {
+	m, _ := NewMachine("s0")
 
-	if err != nil {
-		t.Error("TestAddState cannot construct a new machine")
+	// Adding a state to a started machine should fail.
+	m.Start()
+	err := m.AddState("s1")
+
+	if err == nil {
+		t.Error("AddState allowed mutation of a started machine")
 	}
 
-	if m.current == nil || m.current.name != "s0" {
-		t.Error("TestAddState constructed an invalid machine")
+	// Adding a duplicate state should fail.
+	m, _ = NewMachine("s0")
+	err = m.AddState("s0")
+
+	if err == nil {
+		t.Error("AddState allowed a duplicate state to be added")
 	}
+
+	m, _ = NewMachine("s0")
 
 	if m.initial == nil || m.initial.name != "s0" {
-		t.Error("TestAddState constructed an invalid machine")
+		t.Error("AddState did not set the initial state")
+	}
+}
+
+func TestMachine_AddTransition(t *testing.T) {
+	// TODO
+}
+
+func TestMachine_Current(t *testing.T) {
+	m, _ := NewMachine("s0")
+
+	if m.current.name != m.Current() {
+		t.Error("Current did not return the name of the current state")
+	}
+}
+
+func TestMachine_Start(t *testing.T) {
+	m, _ := NewMachine("s0")
+	m.Start()
+
+	// Starting a machine should set its current state to its initial state.
+	if m.current != m.initial {
+		t.Error("Start did not transition the machine into its initial state")
 	}
 
-	if m.initial.name != m.current.name {
-		t.Error("TestAddState constructed an invalid machine")
-	}
+	// A started machine cannot be started again.
+	err := m.Start()
 
-	stateCount := len(m.states)
-	err = m.AddState("s1")
-
-	if err != nil || len(m.states) != stateCount+1 {
-		t.Error("TestAddState cannot add a new state")
-	}
-
-	if len(m.states) < 2 {
-		t.Error("TestAddState failed to add a new state")
+	if err == nil {
+		t.Error("Start allowed a started machine to be started")
 	}
 }
