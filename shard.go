3288ad9f20333258c86c7ab375e38f8ec88cd681
Nick Miller <nick@nmiller.info>
2018-12-02 03:07:05 -0500

    Move shard stuff into separate file

---
+++ shard.go
@@ -0,0 +1,36 @@
+package avatar
+
+import (
+	"math/rand"
+	"net"
+	"time"
+)
+
+// ShardNameLength is the maximum number of US ASCII characters allowed to
+// use as a shard name.
+const ShardNameLength = 32
+
+// ShardService provides methods for retrieving shards.
+type ShardService interface {
+	All() []Shard
+	Find(name string) (*Shard, error)
+}
+
+// Shard contains information about a game server.
+type Shard struct {
+	Record
+
+	Name      string
+	TimeZone  string
+	Capacity  int
+	IPAddress net.IP
+
+	Location time.Location
+}
+
+// PercentFull returns how full the shard is based on the number of clients
+// in the game world and the shard's capacity.
+func (s Shard) PercentFull() int32 {
+	// TODO: len(clients) / s.Capacity
+	return rand.Int31n(100)
+}
