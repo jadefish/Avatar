23f06073d91461f3ade4bd1b512a1a06121c7a54
Nick Miller <nick@nmiller.info>
2019-03-24 02:26:33 -0400

    Clean up DB initialization
    * Move initialization "migration" to the PG image's initialization
      scripts directory
    * Fix data directory name within container
    * Ensure correct database is operated on when running initialization
      script
    * Store password in /run/secrets

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,31 +1,29 @@
-BEGIN;
-
-CREATE OR REPLACE LANGUAGE 'plpgsql';
+CREATE OR REPLACE LANGUAGE plpgsql;
 
 -- Trigger for updating `updated_at` columns:
 CREATE OR REPLACE FUNCTION on_update_set_updated_at()
 RETURNS TRIGGER AS $$
 BEGIN
-	NEW.updated_at = transaction_timestamp();
-	RETURN new;
+    NEW.updated_at = transaction_timestamp();
+    RETURN new;
 END;
 $$ language 'plpgsql';
 
 -- Domain for email addresses:
 CREATE EXTENSION IF NOT EXISTS plperl;
-CREATE OR REPLACE LANGUAGE 'plperlu';
+CREATE OR REPLACE LANGUAGE plperlu;
 
 CREATE OR REPLACE FUNCTION valid_email(text) RETURNS boolean
 LANGUAGE 'plperlu'
 IMMUTABLE LEAKPROOF STRICT AS $$
-	use Email::Valid;
-	my $email = shift;
-	Email::Valid->address($email) or die "Invalid email address: $email\n";
-	return 'true';
+    use Email::Valid;
+    my $email = shift;
+    Email::Valid->address($email) or die "Invalid email address: $email\n";
+    return 'true';
 $$;
 
 CREATE DOMAIN validemail AS text NOT NULL
-	CONSTRAINT validemail_check CHECK (valid_email(VALUE));
+    CONSTRAINT validemail_check CHECK (valid_email(VALUE));
 
 -- Domain for time zones:
 CREATE OR REPLACE FUNCTION valid_time_zone(tz text) RETURNS BOOLEAN
@@ -42,5 +40,3 @@ $$;
 
 CREATE DOMAIN timezone AS text NOT NULL
     CONSTRAINT timezone_check CHECK (valid_time_zone(VALUE));
-
-COMMIT;
