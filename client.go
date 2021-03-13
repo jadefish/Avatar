38c109913fb63c9c9e28415c0aaf5691e1d63d14
Nick Miller <nick@nmiller.info>
2021-03-12 21:40:30 -0500

    Remove dead code

--- client.go
+++ client.go
@@ -1,71 +1,38 @@
 package avatar
 
 import (
-	"bytes"
-	"encoding/binary"
 	"fmt"
 )
 
-type ClientState uint8
-
-// Client states
-const (
-	StateDisconnected ClientState = iota
-	StateConnected
-	StateAuthenticating
-	StateAuthenticated
-	StateLoggedIn
-)
-
-// ClientVersion represents a client's self-declared version.
-type ClientVersion struct {
+// Version identifies a unique client revision.
+type Version struct {
 	Major    uint32
 	Minor    uint32
 	Patch    uint32
 	Revision uint32
 }
 
-func (v ClientVersion) MarshalBinary() ([]byte, error) {
-	var b bytes.Buffer
-
-	binary.Write(&b, Encoding, v.Major)
-	binary.Write(&b, Encoding, v.Minor)
-	binary.Write(&b, Encoding, v.Patch)
-	binary.Write(&b, Encoding, v.Revision)
-
-	return b.Bytes(), nil
-}
-
-func (v *ClientVersion) UnmarshalBinary(data []byte) error {
-	r := bytes.NewReader(data)
-	err := binary.Read(r, Encoding, &v.Major)
-	err = binary.Read(r, Encoding, &v.Minor)
-	err = binary.Read(r, Encoding, &v.Patch)
-	err = binary.Read(r, Encoding, &v.Revision)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (v ClientVersion) String() string {
+func (v Version) String() string {
 	return fmt.Sprintf("%d.%d.%d.%d", v.Major, v.Minor, v.Patch, v.Revision)
 }
 
-type hasVersion interface {
-	Version() ClientVersion
-	SetVersion(ClientVersion)
-}
+// ClientState captures the current state of the client.
+type ClientState int
 
-type hasCryptoService interface {
-	Crypto() CryptoService
-	SetCrypto(CryptoService)
-}
+const (
+	// ClientStateUnknown indicates that the current state of the client is not
+	// currently known. It is not safe to assume the client can be used.
+	ClientStateUnknown ClientState = iota
 
-// Client is a representation of a connected user.
-type Client interface {
-	hasVersion
-	hasCryptoService
-}
+	// ClientStateDisconnected indicates that the client's connection to the
+	// server is no longer valid.
+	ClientStateDisconnected
+
+	// ClientStateNew indicates that a client has just recently connected and
+	// has not yet been authenticated.
+	ClientStateNew
+
+	// ClientStateAuthenticated describes a client has been successfully
+	// authenticated.
+	ClientStateAuthenticated
+)
